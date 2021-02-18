afa03f9138a4175ef9166d47436d26f0c32ed942
René Hansen <rene@hansen-audio.de>
2021-02-18 16:41:30 +0100

    Correct type in pragma once.

--- include/ha/ptb/paramramp.h
+++ include/ha/ptb/paramramp.h
@@ -1,30 +1,30 @@
 // Copyright(c) 2021 Hansen Audio.
 
-#pragma one
+#pragma once
 
 namespace HA {
 namespace PTB {
+
 //------------------------------------------------------------------------
 //  ParamRamp
 //------------------------------------------------------------------------
-class ParamRamp
-{
+class ParamRamp {
 public:
-    //------------------------------------------------------------------------
-    using ValueType     = const float;
-    using mut_ValueType = float;
+  //------------------------------------------------------------------------
+  using ValueType = const float;
+  using mut_ValueType = float;
 
-    ParamRamp(ValueType src, ValueType dst, int _numSamples);
-    ValueType tick();
-    ValueType getValue() const { return x; };
-    bool isDone() const;
+  ParamRamp(ValueType src, ValueType dst, int _numSamples);
+  ValueType tick();
+  ValueType getValue() const { return x; };
+  bool isDone() const;
 
-    //------------------------------------------------------------------------
+  //------------------------------------------------------------------------
 private:
-    mut_ValueType dst   = 0.;
-    mut_ValueType delta = 0.1;
-    mut_ValueType x     = 0.;
-    int numSamples      = 32;
+  mut_ValueType dst = 0.;
+  mut_ValueType delta = 0.1;
+  mut_ValueType x = 0.;
+  int numSamples = 32;
 };
 
 //-----------------------------------------------------------------------------
