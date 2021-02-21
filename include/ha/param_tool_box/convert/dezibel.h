184870fc3228292dfc2a71e7aed9ebf87aa1ffd3
René Hansen <rene@hansen-audio.de>
2021-02-21 17:43:40 +0100

    Clean up clamp code.

--- include/ha/param_tool_box/convert/dezibel.h
+++ include/ha/param_tool_box/convert/dezibel.h
@@ -24,6 +24,18 @@ std::string to_string_with_precision(T const a_value, int const n = 6)
 }
 
 //-----------------------------------------------------------------------------
+template <class T>
+constexpr const T& clamp(const T& v, const T& lo, const T& hi)
+{
+#if __cplusplus < 201700
+    T const tmp_v = std::max(v, lo);
+    return std::min(tmp_v, hi);
+#else
+    return std::clamp(v, lo, hi);
+#endif
+}
+
+//-----------------------------------------------------------------------------
 } // namespace
 
 //-----------------------------------------------------------------------------
@@ -52,33 +64,26 @@ public:
         static value_type const kExp     = kReciprocal20 * min_dB;
         static value_type const kNormMin = pow(kBase, kExp);
 
-#if __cplusplus < 201700
         normalized = std::max(normalized, kNormMin);
         normalized = std::min(normalized, value_type(1));
-#else
-        normalized = std::clamp(normalized, kNormMin, value_type(1.));
-#endif
+        normalized = clamp(normalized, kNormMin, value_type(1.));
+
         return value_type(20.) * log10(normalized);
     }
 
     value_type toNormalized(value_type physical) const
     {
-#if __cplusplus < 201700
-        physical = std::max(physical, min_dB);
-        physical = std::min(physical, max_dB);
-#else
-        physical   = std::clamp(physical, min_dB, max_dB);
-#endif
+        physical = clamp(physical, min_dB, max_dB);
+
         return pow(value_type(10.), kReciprocal20 * physical);
     }
 
     string_type toString(value_type physical, fn_precision const& precision_func = nullptr) const
     {
-        int precision = kStandardPrecision;
-        if (precision_func)
-            precision = precision_func(physical);
+        value_type const tmp_physical = clamp(physical, min_dB, max_dB);
+        int const precision = precision_func ? precision_func(tmp_physical) : kStandardPrecision;
 
-        return physical <= min_dB ? "-inf" : to_string_with_precision(physical, precision);
+        return tmp_physical <= min_dB ? "-inf" : to_string_with_precision(tmp_physical, precision);
     }
 
     value_type fromString(string_type const& value_string) const
@@ -86,6 +91,7 @@ public:
         // TODO: Make this more robust to non-digit inputs.
         value_type const value =
             value_string == "-inf" ? value_type(min_dB) : value_type(std::stod(value_string));
+
         return value;
     }
 
