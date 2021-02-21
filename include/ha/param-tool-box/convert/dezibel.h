66ed2a7ac96fe479e76627e464c1278eae2ed48f
René Hansen <rene@hansen-audio.de>
2021-02-21 10:02:38 +0100

    Rename members. Make class final.

--- include/ha/param-tool-box/convert/dezibel.h
+++ include/ha/param-tool-box/convert/dezibel.h
@@ -30,7 +30,7 @@ std::string to_string_with_precision(const T a_value, const int n = 6)
 // Dezibel
 //-----------------------------------------------------------------------------
 template <typename tRealType>
-class Dezibel
+class Dezibel final
 {
 public:
     //-------------------------------------------------------------------------
@@ -40,16 +40,16 @@ public:
 
     static const int kStandardPrecision = 2;
 
-    Dezibel(value_type min, value_type max)
-    : kMin(min)
-    , kMax(max)
+    Dezibel(value_type min_dB, value_type max_dB)
+    : min_dB(min_dB)
+    , max_dB(max_dB)
     {
     }
 
     value_type toPhysical(value_type normalized) const
     {
         static constexpr value_type kBase = 10.;
-        static const value_type kExp      = (value_type(1.) / value_type(20.)) * kMin;
+        static const value_type kExp      = (value_type(1.) / value_type(20.)) * min_dB;
         static const value_type kNormMin  = pow(kBase, kExp);
 
 #if __cplusplus < 201700
@@ -65,9 +65,9 @@ public:
     {
 #if __cplusplus < 201700
         physical = std::max(physical, kMin);
-        physical = std::min(physical, kMax);
+        physical = std::min(physical, max_dB);
 #else
-        physical   = std::clamp(physical, kMin, kMax);
+        physical   = std::clamp(physical, min_dB, max_dB);
 #endif
         return pow(value_type(10.), value_type(1.) / value_type(20.) * physical);
     }
@@ -78,21 +78,21 @@ public:
         if (func)
             precision = func(physical);
 
-        return physical <= kMin ? "-inf" : to_string_with_precision(physical, precision);
+        return physical <= min_dB ? "-inf" : to_string_with_precision(physical, precision);
     }
 
     value_type fromString(const StringType& string) const
     {
         // TODO: Make this more robust to non-digit inputs.
         const value_type value =
-            string == "-inf" ? value_type(kMin) : value_type(std::stod(string));
+            string == "-inf" ? value_type(min_dB) : value_type(std::stod(string));
         return value;
     }
 
     //-------------------------------------------------------------------------
 private:
-    value_type kMin = -96.;
-    value_type kMax = 0.;
+    value_type min_dB = -96.;
+    value_type max_dB = 0.;
 };
 
 //-----------------------------------------------------------------------------
