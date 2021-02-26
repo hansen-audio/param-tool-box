68cdaf82c6f74bc2afe008df0431cab2b4f7fcfc
René Hansen <rene@hansen-audio.de>
2021-02-26 19:06:29 +0100

    Move implementations to source.

--- include/ha/param_tool_box/convert/dezibel.h
+++ include/ha/param_tool_box/convert/dezibel.h
@@ -53,52 +53,76 @@ public:
     static value_type constexpr RECIPROCAL_20 = value_type(1.) / value_type(20.);
     static i32 const STANDARD_PRECISION       = 2;
 
-    dezibel(value_type lo, value_type hi)
-    : lo(lo)
-    , hi(hi)
-    {
-    }
+    dezibel(value_type lo, value_type hi);
 
-    value_type to_physical(value_type normalized) const
-    {
-        static value_type const BASE           = value_type(10.);
-        static value_type const EXPONENT       = RECIPROCAL_20 * lo;
-        static value_type const NOMRALIZED_MIN = pow(BASE, EXPONENT);
+    value_type to_physical(value_type normalized) const;
 
-        normalized = clamp(normalized, NOMRALIZED_MIN, value_type(1.));
+    value_type to_normalized(value_type physical) const;
 
-        return value_type(20.) * log10(normalized);
-    }
+    string_type to_string(value_type physical, fn_precision const& precision_func = nullptr) const;
 
-    value_type to_normalized(value_type physical) const
-    {
-        physical = clamp(physical, lo, hi);
-
-        return pow(value_type(10.), RECIPROCAL_20 * physical);
-    }
-
-    string_type to_string(value_type physical, fn_precision const& precision_func = nullptr) const
-    {
-        value_type const tmp_physical = clamp(physical, lo, hi);
-        i32 const precision = precision_func ? precision_func(tmp_physical) : STANDARD_PRECISION;
-
-        return tmp_physical <= lo ? "-inf" : to_string_with_precision(tmp_physical, precision);
-    }
-
-    value_type from_string(string_type const& value_string) const
-    {
-        // TODO: Make this more robust to non-digit inputs.
-        value_type const value =
-            value_string == "-inf" ? value_type(lo) : value_type(std::stod(value_string));
-
-        return clamp(value, lo, hi);
-    }
+    value_type from_string(string_type const& value_string) const;
 
     //-------------------------------------------------------------------------
 private:
     value_type lo = -96.;
     value_type hi = 0.;
 };
+
+//-----------------------------------------------------------------------------
+//  dezibel
+//-----------------------------------------------------------------------------
+template <typename RealType>
+dezibel<RealType>::dezibel(value_type lo, value_type hi)
+: lo(lo)
+, hi(hi)
+{
+}
+
+//-----------------------------------------------------------------------------
+template <typename RealType>
+typename dezibel<RealType>::value_type dezibel<RealType>::to_physical(value_type normalized) const
+{
+    static value_type const BASE           = value_type(10.);
+    static value_type const EXPONENT       = RECIPROCAL_20 * lo;
+    static value_type const NOMRALIZED_MIN = pow(BASE, EXPONENT);
+
+    normalized = clamp(normalized, NOMRALIZED_MIN, value_type(1.));
+
+    return value_type(20.) * log10(normalized);
+}
+
+//-----------------------------------------------------------------------------
+template <typename RealType>
+typename dezibel<RealType>::value_type dezibel<RealType>::to_normalized(value_type physical) const
+{
+    physical = clamp(physical, lo, hi);
+
+    return pow(value_type(10.), RECIPROCAL_20 * physical);
+}
+
+//-----------------------------------------------------------------------------
+template <typename RealType>
+typename dezibel<RealType>::string_type
+dezibel<RealType>::to_string(value_type physical, fn_precision const& precision_func) const
+{
+    value_type const tmp_physical = clamp(physical, lo, hi);
+    i32 const precision = precision_func ? precision_func(tmp_physical) : STANDARD_PRECISION;
+
+    return tmp_physical <= lo ? "-inf" : to_string_with_precision(tmp_physical, precision);
+}
+
+//-----------------------------------------------------------------------------
+template <typename RealType>
+typename dezibel<RealType>::value_type
+dezibel<RealType>::from_string(string_type const& value_string) const
+{
+    // TODO: Make this more robust to non-digit inputs.
+    value_type const value =
+        value_string == "-inf" ? value_type(lo) : value_type(std::stod(value_string));
+
+    return clamp(value, lo, hi);
+}
 
 //-----------------------------------------------------------------------------
 } // namespace convert
